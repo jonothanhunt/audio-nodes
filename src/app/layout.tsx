786d12fbd99595c8b64f99252d2906b6c3099328
Jonothan Hunt <hey@jonothan.dev>
2025-08-12 15:44:52 +0100

    Add info pages and centralise layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import localFont from "next/font/local";
 import "./globals.css";
+import TitleBarCreds from "@/components/TitleBarCreds";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -38,8 +39,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} ${atkinsonHyperlegible.variable} ${lastik.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${atkinsonHyperlegible.variable} ${lastik.variable} antialiased bg-gray-900 text-white`}
       >
+        <div className="fixed top-4 left-4 right-4 z-[70] pointer-events-auto">
+          <TitleBarCreds />
+        </div>
         {children}
       </body>
     </html>
